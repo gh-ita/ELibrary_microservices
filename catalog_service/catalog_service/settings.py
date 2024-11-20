ed74d3d9c8e700aa13921db7fb9e007ea0c86a7c
gh-ita <hatimi.ghita@ensam-casa.ma>
2024-11-20 11:11:50 +0100

    intregrated postgresql to the catalog service

--- catalog_service/catalog_service/settings.py
+++ catalog_service/catalog_service/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +76,14 @@ WSGI_APPLICATION = "catalog_service.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.getenv('POSTGRES_DB', 'catalogdb'),
+    'USER': os.getenv('POSTGRES_USER', 'ghita'),
+    'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'ghita'),
+    'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+    'PORT': os.getenv('POSTGRES_PORT', '5432'),
+}
 }
 
 
